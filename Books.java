bbd5afc1b28c623a8230fbedd314ee508ce0922d
SafaAl <127904342+SafaAL1@users.noreply.github.com>
2023-03-26 06:39:33 -0500

    Create Books.java

---
+++ Books.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coe528finalphase;
+
+import javafx.scene.control.CheckBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author Safa
+ */
+public class Books {
+    public String Name; 
+    public int Price; 
+    int amountOfBooks;
+    public CheckBox select;
+    
+    public Books(String Name,int Price){
+        this.Name = Name; 
+        this.Price =Price; 
+        amountOfBooks=1;
+        this.select = new CheckBox();      
+        
+    }
+    public String getName(){
+        return Name;
+    }
+    public int getPrice(){
+        return Price;
+    }
+    public CheckBox getSelect(){
+        return select;
+    }
+    @Override
+    public String toString(){
+        return Name + " $" + Price;
+    }
+    
+}
