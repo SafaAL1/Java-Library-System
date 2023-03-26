27adb2bd69abab929811f980083462fa716dbd8e
SafaAl <127904342+SafaAL1@users.noreply.github.com>
2023-03-26 06:44:21 -0500

    Create SliverState.java

---
+++ SliverState.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe528finalphase;
+
+public class SilverState extends State{
+    @Override
+    public String getState() {return ("Silver");    }
+}
