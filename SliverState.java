b0d87e36d59f517d7d083a6ac5e18a14e4f7c67c
SafaAl <127904342+SafaAL1@users.noreply.github.com>
2023-03-26 06:45:06 -0500

    Update SliverState.java

--- SliverState.java
+++ SliverState.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package coe528finalphase;
+package finalphase;
 
 public class SilverState extends State{
     @Override
